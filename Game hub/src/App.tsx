63438aa294dafec3d3d989399db7f6bab4d701e8
Gowthami <gowthami.ggowda26@gmail.com>
2024-05-02 12:32:33 +0530

    install chakra ui

--- Game hub/src/App.tsx
+++ Game hub/src/App.tsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Button, ButtonGroup, Grid, grid } from "@chakra-ui/react";
 import "./App.css";
 import { ChakraProvider } from "@chakra-ui/react";
+
 function App() {
   // const [count, setCount] = useState(0);
-  return <button>button</button>;
+  return <grid></grid>;
+  //  <button color="blue">button</button>;
 }
 //   return (
 //     <>
